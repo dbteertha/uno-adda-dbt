4150dbadf4095c005917b6239643d2e15b75ba6d
dbteertha <dbteertha@gmail.com>
2026-09-12 23:05:37 +0600

    Wire active player presence into DBT Games

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createUnoServer } from "./server.js";
 import { registerUnoFlex } from "./UnoFlex.js";
+import { registerPresence } from "./Presence.js";
 
 const server = createUnoServer();
 registerUnoFlex(server.io);
+registerPresence(server.io);
 
 const port = Number(process.env.PORT ?? 3000);
 server.http.listen(port, "0.0.0.0", () => console.log(`DBT Games server listening on port ${port}`));
